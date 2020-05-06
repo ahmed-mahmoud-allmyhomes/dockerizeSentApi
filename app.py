0af5b4f2357ccae2a3a7706827c7f39d5307a94b
Ahmed Mahmoud <ahmed.mahmoud@LPT-A2141-10.local>
2020-05-06 19:25:08 +0200

    update readme

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, abort, Api, Resource, request
 import pickle
 import numpy as np
 from model import NLPModel
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,18 +17,15 @@ with open(clf_path, 'rb') as f:
 vec_path = 'lib/models/TFIDFVectorizer.pkl'
 with open(vec_path, 'rb') as f:
     model.vectorizer = pickle.load(f)
-
-# argument parsing
 parser = reqparse.RequestParser()
 parser.add_argument('query')
-
 
 class PredictSentiment(Resource):
     def get(self):
         # use parser and find the user's query
         args = parser.parse_args()
         user_query = args['query']
-
+        
         # vectorize the user's query and make a prediction
         uq_vectorized = model.vectorizer_transform(np.array([user_query]))
         prediction = model.predict(uq_vectorized)
@@ -48,14 +46,14 @@ class PredictSentiment(Resource):
         return output
 
 
+# argument parsing
+
+
+
+
 # Setup the Api resource routing here
 # Route the URL to the resource
 api.add_resource(PredictSentiment, '/')
-
-@app.route('/greeting')
-def query_result():
-    predict = PredictSentiment
-    return predict
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
